22e7588dade91bfb0be39fa9ce0e3a4c5e354e8a
hexiaofeng666 <1252533955@qq.com>
2023-07-24 17:14:54 +0800

    0724

--- src/components/sceneRoam.vue
+++ src/components/sceneRoam.vue
@@ -4,8 +4,9 @@
 
 <script lang="ts" setup>
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { onMounted, ref, onBeforeUnmount } from 'vue'
 
@@ -28,19 +29,15 @@ const width = window.innerWidth
 const height = window.innerHeight
 //创建一个透视相机，窗口宽度，窗口高度
 const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000)
-camera.position.set(-45, 60, 457)
+camera.position.set(-45, 600, 457)
 camera.lookAt(0, 60, 0)
 console.log(camera);
-const cameraDirection = new THREE.Vector3();
-console.log(camera.getWorldDirection(cameraDirection));
-
-
 
 
 
 //创建辅助坐标轴
-const axesHelper = new THREE.AxesHelper(200) //参数200标示坐标系大小，可以根据场景大小去设置
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper(200) //参数200标示坐标系大小，可以根据场景大小去设置
+// scene.add(axesHelper)
 
 
 const renderer = new THREE.WebGLRenderer()
@@ -48,63 +45,158 @@ renderer.setSize(width, height)
 renderer.setClearColor(0xffffff, 1) // 设置背景颜色为白色
 renderer.render(scene, camera)
 
-// 添加轨道控制器
-// const controls = new OrbitControls(camera, renderer.domElement) //创建控件对象
-// controls.addEventListener('change', () => {
-//     console.log(camera);
+// 创建PointerLockControls
+const controls = new PointerLockControls(camera, renderer.domElement);
 
-//     renderer.render(scene, camera) //监听鼠标，键盘事件
-// })
+renderer.domElement.addEventListener('click', function () {
+    controls.lock();
+});
 
-// 创建FirstPersonControls
-const controls = new FirstPersonControls(camera, renderer.domElement);
-controls.movementSpeed = 0.1;
-controls.lookSpeed = 0.1;
-// controls.addEventListener('change', () => {
-//     console.log(camera);
+controls.addEventListener('lock', function () {
+    camera.position.set(-45, 600, 457)
+    // renderer.domElement.style.display = 'none';
+});
 
-//     renderer.render(scene, camera) //监听鼠标，键盘事件
-// })
+// controls.addEventListener('unlock', function () {
+//     renderer.domElement.style.display = '';
+// });
 
-const moveSpeed = 0.1 // 移动速度
-function onKeyDown(event: any) {
-    console.log(event);
+scene.add(controls.getObject());
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const onKeyDown = function (event: { code: any; }) {
+
     switch (event.code) {
-        case 'ArrowUp':
-            camera.position.z += 1
-            break;
-        case 'ArrowDown':
-            camera.position.z -= 1
-            break;
-        case 'ArrowLeft':
-            camera.position.x += 1
-            break;
-        case 'ArrowRight':
-            camera.position.x -= 1
-            break;
-        case 'KeyJ':
-            camera.position.y += 1
-            break;
-        case 'KeyK':
-            camera.position.y -= 1
-            break;
-        default:
-            break;
-    }
-    renderer.render(scene, camera)
-}
 
-// 监听键盘事件
-window.addEventListener('keydown', onKeyDown)
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+
+        case 'Space':
+            if (canJump === true) velocity.y += 350;
+            canJump = false;
+            break;
+
+    }
+
+};
+
+const onKeyUp = function (event: { code: any; }) {
+
+    switch (event.code) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+
+    }
+
+};
+
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
 
 // 渲染
 const clock = new THREE.Clock()
-const render = () => {
-        const delta = clock.getDelta();
-        controls.update(delta)
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
-      };
+let raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+let objects: never[] = [];
+let prevTime = performance.now();
+function render() {
+
+requestAnimationFrame( render );
+
+const time = performance.now();
+
+if ( controls.isLocked === true ) {
+
+    raycaster.ray.origin.copy( controls.getObject().position );
+    raycaster.ray.origin.y -= 10;
+
+    const intersections = raycaster.intersectObjects( objects, false );
+
+    const onObject = intersections.length > 0;
+
+    const delta = ( time - prevTime ) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    direction.z = Number( moveForward ) - Number( moveBackward );
+    direction.x = Number( moveRight ) - Number( moveLeft );
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+    if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+    if ( onObject === true ) {
+
+        velocity.y = Math.max( 0, velocity.y );
+        canJump = true;
+
+    }
+
+    controls.moveRight( - velocity.x * delta );
+    controls.moveForward( - velocity.z * delta );
+
+    controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+    if ( controls.getObject().position.y < 10 ) {
+
+        velocity.y = 0;
+        controls.getObject().position.y = 10;
+
+        canJump = true;
+
+    }
+
+}
+
+prevTime = time;
+
+renderer.render( scene, camera );
+
+}
+
 
 const box = ref()
 onMounted(() => {
@@ -113,6 +205,6 @@ onMounted(() => {
     //   document.getElementById('my-three')?.appendChild(renderer.domElement)
 })
 onBeforeUnmount(() => {
-        renderer.dispose();
-      });
+    renderer.dispose();
+});
 </script>
